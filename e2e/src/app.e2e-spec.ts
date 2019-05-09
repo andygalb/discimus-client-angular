f898431ed3cf753c11cb314f3fb80bb8dfb37443
Andrew Galbraith <andy_galb@hotmail.com>
2019-05-09 14:44:06 +0200

    Linting.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display title as Discimus', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to ltc-app!');
+    expect(page.getTitleText()).toEqual('Discimus');
   });
 });
